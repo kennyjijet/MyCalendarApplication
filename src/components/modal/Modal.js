2f579d599e187be8da776d14489506456e8ae61f
Justin Jetwiriyanon <jittarinjetwiriyanon@justins-MacBook-Air.local>
2019-11-04 01:23:21 +1300

    add modal

---
+++ src/components/modal/Modal.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './style/Modal.scss';
+
+class Modal extends React.Component {
+    render() {
+        return (
+            <div className="modalCustomized">
+
+            </div>
+        );
+    }
+}
+
+export default Modal
